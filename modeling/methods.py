d7f0f9b586939af26ef6aca12c542fa49b26a89e
Toby Wong <twong8954@gmail.com>
2026-04-06 19:15:50 -0400

    update to modeling methods

--- modeling/methods.py
+++ modeling/methods.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import os
 
-def ETL(os_path, csv_filename, stateid, drop_columns, keep_columns):       
-    #This method will take the raw data from your original data retrieval file and creates your specific modeling dataset
-    #This method only adds together 2 columns. Modify it if you need more
+def ETL(os_path, csv_filename, stateid, drop_columns, keep_columns):
     full_path = os.path.join(os_path, csv_filename)
     df_raw = pd.read_csv(full_path)
-    df_raw = df_raw[df_raw['stateid'] == stateid]
-    df_dropped = df_raw.drop(columns=drop_columns)
-    df_combined = df_dropped.copy()
-    df_combined[keep_columns[0] + '_' + keep_columns[1]] = df_dropped[keep_columns[0]] + df_dropped[keep_columns[1]]
-    df_combined = df_combined.drop(columns=[keep_columns[0], keep_columns[1], "stateDescription"])
-    return df_combined
+
+    df_filtered = df_raw[df_raw["stateid"] == stateid]
+    df_result = df_filtered.drop(columns=drop_columns, errors="ignore").copy()
+
+    if len(keep_columns) > 1:
+        new_col_name = "_".join(keep_columns)
+        df_result[new_col_name] = (
+            df_result[keep_columns]
+            .astype(str)
+            .agg("".join, axis=1)
+        )
+        df_result = df_result.drop(columns=keep_columns, errors="ignore")
+
+    df_result = df_result.drop(columns=["stateDescription"], errors="ignore")
+
+    return df_result
